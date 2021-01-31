ad7da041b2a4c49b777535b575abd602cfde864e
raphzahn <rz271994@gmail.com>
2021-01-31 23:49:29 +0100

    create XLSX and CSV

--- index.js
+++ index.js
@@ -20,18 +20,106 @@ const combine = function(file) {
 const createTxt = function(data){
 	for(var i = 0;i < data.length;i++){
 		if(!dataTxt.includes(data[i].Vermittler)){
-			dataTxt.push(data[i].Vermittler);
+			dataTxtTemp1.push(data[i].Vermittler);
 		}
 	}
+	var dataTxtTemp2 = JSON.stringify(dataTxtTemp1);
+	var dataTxtTemp3 = dataTxtTemp2.substring(2);
+	var dataTxt = dataTxtTemp3.slice(0, -2);
 }
 
-const createXlsx = function(data){
+const createXlsxAndCsv = function(data){
 	for(var i = 0;i < data.length;i++){
 		Json = [];
 		Json["VM-Policeninfo"] = data[i]["VM-Policeninfo"];
-		Json["Vermittler"] = data[i]["Vermittlers"];
+		Json["Vermittler"] = data[i]["Vermittler"];
+		Json["Quittungs-Nr."] = data[i]["Quittungs-Nr."];
+		Json["Sprache"] = data[i]["Sprache"];
+		Json["Anrede"] = data[i]["Anrede"];
+		Json["Name"] = data[i]["Name"];
+		Json["Vorname"] = data[i]["Vorname"];
+		Json["Name2"] = data[i]["Name2"];
+		Json["Strasse"] = data[i]["Strasse"];
+		Json["Postfach"] = data[i]["Postfach"];
+		Json["PLZ/Ort"] = data[i]["PLZ/Ort"];
+		Json["Policen-Nr.2"] = data[i]["Policen-Nr.2"];
+		Json["Produkt2"] = data[i]["Produkt2"];
+		Json["Gewinn2"] = data[i]["Gewinn2"];
+		Json["Policen-Nr.2"] = data[i]["Policen-Nr.2"];
+		Json["Produkt2"] = data[i]["Produkt2"];
+		Json["Gewinn2"] = data[i]["Gewinn2"];
+		Json["Policen-Nr.3"] = data[i]["Policen-Nr.3"];
+		Json["Produkt3"] = data[i]["Produkt3"];
+		Json["Gewinn3"] = data[i]["Gewinn3"];
+		Json["Policen-Nr.4"] = data[i]["Policen-Nr.4"];
+		Json["Produkt4"] = data[i]["Produkt4"];
+		Json["Gewinn4"] = data[i]["Gewinn4"];
+		Json["Policen-Nr.5"] = data[i]["Policen-Nr.5"];
+		Json["Produkt5"] = data[i]["Produkt5"];
+		Json["Gewinn5"] = data[i]["Gewinn5"];
+		Json["Policen-Nr.6"] = data[i]["Policen-Nr.6"];
+		Json["Produkt6"] = data[i]["Produkt6"];
+		Json["Gewinn6"] = data[i]["Gewinn6"];
+		Json["Policen-Nr.7"] = data[i]["Policen-Nr.7"];
+		Json["Produkt7"] = data[i]["Produkt7"];
+		Json["Gewinn7"] = data[i]["Gewinn7"];
+		Json["Policen-Nr.8"] = data[i]["Policen-Nr.8"];
+		Json["Produkt8"] = data[i]["Produkt8"];
+		Json["Gewinn8"] = data[i]["Gewinn8"];
+		Json["Policen-Nr.9"] = data[i]["Policen-Nr.9"];
+		Json["Produkt9"] = data[i]["Produkt9"];
+		Json["Gewinn9"] = data[i]["Gewinn9"];
+		Json["Policen-Nr.20"] = data[i]["Policen-Nr.20"];
+		Json["Produkt20"] = data[i]["Produkt20"];
+		Json["Gewinn20"] = data[i]["Gewinn20"];
+		Json["Policen-Nr.22"] = data[i]["Policen-Nr.22"];
+		Json["Produkt22"] = data[i]["Produkt22"];
+		Json["Gewinn22"] = data[i]["Gewinn22"];
+		Json["Policen-Nr.22"] = data[i]["Policen-Nr.22"];
+		Json["Produkt22"] = data[i]["Produkt22"];
+		Json["Gewinn22"] = data[i]["Gewinn22"];
+		Json["Policen-Nr.23"] = data[i]["Policen-Nr.23"];
+		Json["Produkt23"] = data[i]["Produkt23"];
+		Json["Gewinn23"] = data[i]["Gewinn23"];
+		Json["Policen-Nr.24"] = data[i]["Policen-Nr.24"];
+		Json["Produkt24"] = data[i]["Produkt24"];
+		Json["Gewinn24"] = data[i]["Gewinn24"];
+		Json["Policen-Nr.25"] = data[i]["Policen-Nr.25"];
+		Json["Produkt25"] = data[i]["Produkt25"];
+		Json["Gewinn25"] = data[i]["Gewinn25"];
+		Json["Policen-Nr.16"] = data[i]["Policen-Nr.16"];
+		Json["Produkt16"] = data[i]["Produkt16"];
+		Json["Gewinn16"] = data[i]["Gewinn16"];
+		Json["Policen-Nr.17"] = data[i]["Policen-Nr.17"];
+		Json["Produkt17"] = data[i]["Produkt17"];
+		Json["Gewinn17"] = data[i]["Gewinn17"];
+		Json["Policen-Nr.18"] = data[i]["Policen-Nr.18"];
+		Json["Produkt18"] = data[i]["Produkt18"];
+		Json["Gewinn18"] = data[i]["Gewinn18"];
+		Json["Policen-Nr.19"] = data[i]["Policen-Nr.19"];
+		Json["Produkt19"] = data[i]["Produkt19"];
+		Json["Gewinn19"] = data[i]["Gewinn19"];
+		Json["Policen-Nr.20"] = data[i]["Policen-Nr.20"];
+		Json["Produkt20"] = data[i]["Produkt20"];
+		Json["Gewinn20"] = data[i]["Gewinn20"];
+		Json["Policen-Nr.22"] = data[i]["Policen-Nr.22"];
+		Json["Produkt22"] = data[i]["Produkt22"];
+		Json["Gewinn22"] = data[i]["Gewinn22"];
+		Json["Policen-Nr.22"] = data[i]["Policen-Nr.22"];
+		Json["Produkt22"] = data[i]["Produkt22"];
+		Json["Gewinn22"] = data[i]["Gewinn22"];
+		Json["Policen-Nr.23"] = data[i]["Policen-Nr.23"];
+		Json["Produkt23"] = data[i]["Produkt23"];
+		Json["Gewinn23"] = data[i]["Gewinn23"];
+		Json["Policen-Nr.24"] = data[i]["Policen-Nr.24"];
+		Json["Produkt24"] = data[i]["Produkt24"];
+		Json["Gewinn24"] = data[i]["Gewinn24"];
+		Json["Policen-Nr.25"] = data[i]["Policen-Nr.25"];
+		Json["Produkt25"] = data[i]["Produkt25"];
+		Json["Gewinn25"] = data[i]["Gewinn25"];
+		Json["Summe Gewinn"] = data[i]["Summe Gewinn"];
 		dataXlsx.push(Json);
-		console.log(Json);
+		dataCsv.push(Json);
 	}
 }
 
@@ -47,8 +135,10 @@ const readFile = function(files) {
 		};
 		reader.readAsArrayBuffer(f);
 	}
+	const XPORT = document.getElementById('exportBtn');
+	XPORT.disabled = false;
 	createTxt(data);
-	console.log(data);
+	createXlsxAndCsv(data);	
 
 };
 
@@ -65,14 +155,7 @@ const handleReadBtn = async function() {
 	const XPORT = document.getElementById('exportBtn');
 	XPORT.disabled = false;
 	createTxt(data);
-	createXlsx(data);
-	var dataTxt1 = JSON.stringify(dataTxt);
-	var dataTxt2 = dataTxt1.substring(1);
-	var dataTxt3 = dataTxt2.slice(0, -1);
-	console.log(dataTxt3);
-	console.log(dataXlsx)
-	console.log(data);
-	
+	createXlsxAndCsv(data);	
 }
 	
 
@@ -88,7 +171,6 @@ const exportXlsx = async function() {
 	var wb = XLSX.utils.book_new();
 	XLSX.utils.book_append_sheet(wb, XLSX.utils.json_to_sheet(dataXlsx))
 	XLSX.writeFile(wb, o.filePath);
-	electron.dialog.showMessageBox({ message: "Exported data to " + o.filePath, buttons: ["OK"] });
 };
 
 // add event listeners
