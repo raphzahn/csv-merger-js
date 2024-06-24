69066c0a4d4c2396e45bfcb4870792ae8db26553
Raphael Zahner <r.zahner@voegeli.ch>
2024-06-24 14:43:09 +0200

    add Product from 26 to 50

--- src/js/index.js
+++ src/js/index.js
@@ -38,7 +38,7 @@ function createXlsxAndCsv(data){
 		Json = [];
 		Json["VM-Policeninfo"] = data[i]["VM-Policeninfo"];
 		Json["Vermittler"] = data[i]["Vermittler"];
-		Json["Quittungs-Nr."] = data[i]["Quittungs-Nr."];
+		Json["Quittungs-Nr."] = data[i]["Quittungs-Nr"];
 		Json["Sprache"] = data[i]["Sprache"];
 		Json["Anrede"] = data[i]["Anrede"];
 		Json["Name"] = data[i]["Name"];
@@ -122,6 +122,81 @@ function createXlsxAndCsv(data){
 		Json["Policen-Nr.25"] = data[i]["Policen-Nr25"];
 		Json["Produkt25"] = data[i]["Produkt25"];
 		Json["Gewinn25"] = data[i]["Gewinn25"];
+		Json["Policen-Nr.26"] = data[i]["Policen-Nr26"];
+		Json["Produkt26"] = data[i]["Produkt26"];
+		Json["Gewinn26"] = data[i]["Gewinn26"];
+		Json["Policen-Nr.27"] = data[i]["Policen-Nr27"];
+		Json["Produkt27"] = data[i]["Produkt27"];
+		Json["Gewinn27"] = data[i]["Gewinn27"];
+		Json["Policen-Nr.28"] = data[i]["Policen-Nr28"];
+		Json["Produkt28"] = data[i]["Produkt28"];
+		Json["Gewinn28"] = data[i]["Gewinn28"];
+		Json["Policen-Nr.29"] = data[i]["Policen-Nr29"];
+		Json["Produkt29"] = data[i]["Produkt29"];
+		Json["Gewinn29"] = data[i]["Gewinn29"];
+		Json["Policen-Nr.30"] = data[i]["Policen-Nr30"];
+		Json["Produkt30"] = data[i]["Produkt30"];
+		Json["Gewinn30"] = data[i]["Gewinn30"];
+		Json["Policen-Nr.31"] = data[i]["Policen-Nr31"];
+		Json["Produkt31"] = data[i]["Produkt31"];
+		Json["Gewinn31"] = data[i]["Gewinn31"];
+		Json["Policen-Nr.32"] = data[i]["Policen-Nr32"];
+		Json["Produkt32"] = data[i]["Produkt32"];
+		Json["Gewinn32"] = data[i]["Gewinn32"];
+		Json["Policen-Nr.33"] = data[i]["Policen-Nr33"];
+		Json["Produkt33"] = data[i]["Produkt33"];
+		Json["Gewinn33"] = data[i]["Gewinn33"];
+		Json["Policen-Nr.34"] = data[i]["Policen-Nr34"];
+		Json["Produkt34"] = data[i]["Produkt34"];
+		Json["Gewinn34"] = data[i]["Gewinn34"];
+		Json["Policen-Nr.35"] = data[i]["Policen-Nr35"];
+		Json["Produkt35"] = data[i]["Produkt35"];
+		Json["Gewinn35"] = data[i]["Gewinn35"];
+		Json["Policen-Nr.36"] = data[i]["Policen-Nr36"];
+		Json["Produkt36"] = data[i]["Produkt36"];
+		Json["Gewinn36"] = data[i]["Gewinn36"];
+		Json["Policen-Nr.37"] = data[i]["Policen-Nr37"];
+		Json["Produkt37"] = data[i]["Produkt37"];
+		Json["Gewinn37"] = data[i]["Gewinn37"];
+		Json["Policen-Nr.38"] = data[i]["Policen-Nr38"];
+		Json["Produkt38"] = data[i]["Produkt38"];
+		Json["Gewinn38"] = data[i]["Gewinn38"];
+		Json["Policen-Nr.39"] = data[i]["Policen-Nr39"];
+		Json["Produkt39"] = data[i]["Produkt39"];
+		Json["Gewinn39"] = data[i]["Gewinn39"];
+		Json["Policen-Nr.40"] = data[i]["Policen-Nr40"];
+		Json["Produkt40"] = data[i]["Produkt40"];
+		Json["Gewinn40"] = data[i]["Gewinn40"];
+		Json["Policen-Nr.41"] = data[i]["Policen-Nr41"];
+		Json["Produkt41"] = data[i]["Produkt41"];
+		Json["Gewinn41"] = data[i]["Gewinn41"];
+		Json["Policen-Nr.42"] = data[i]["Policen-Nr42"];
+		Json["Produkt42"] = data[i]["Produkt42"];
+		Json["Gewinn42"] = data[i]["Gewinn42"];
+		Json["Policen-Nr.43"] = data[i]["Policen-Nr43"];
+		Json["Produkt43"] = data[i]["Produkt43"];
+		Json["Gewinn43"] = data[i]["Gewinn43"];
+		Json["Policen-Nr.44"] = data[i]["Policen-Nr44"];
+		Json["Produkt44"] = data[i]["Produkt44"];
+		Json["Gewinn44"] = data[i]["Gewinn44"];
+		Json["Policen-Nr.45"] = data[i]["Policen-Nr45"];
+		Json["Produkt45"] = data[i]["Produkt45"];
+		Json["Gewinn45"] = data[i]["Gewinn45"];
+		Json["Policen-Nr.46"] = data[i]["Policen-Nr46"];
+		Json["Produkt46"] = data[i]["Produkt46"];
+		Json["Gewinn46"] = data[i]["Gewinn46"];
+		Json["Policen-Nr.47"] = data[i]["Policen-Nr47"];
+		Json["Produkt47"] = data[i]["Produkt47"];
+		Json["Gewinn47"] = data[i]["Gewinn47"];
+		Json["Policen-Nr.48"] = data[i]["Policen-Nr48"];
+		Json["Produkt48"] = data[i]["Produkt48"];
+		Json["Gewinn48"] = data[i]["Gewinn48"];
+		Json["Policen-Nr.49"] = data[i]["Policen-Nr49"];
+		Json["Produkt49"] = data[i]["Produkt49"];
+		Json["Gewinn49"] = data[i]["Gewinn49"];
+		Json["Policen-Nr.50"] = data[i]["Policen-Nr50"];
+		Json["Produkt50"] = data[i]["Produkt50"];
+		Json["Gewinn50"] = data[i]["Gewinn50"];
 		Json["Summe Gewinn"] = data[i]["Summe Gewinn"];
 		dataXlsx.push(Json);
 		dataCsv.push(Json);
@@ -155,7 +230,7 @@ async function handleReadBtn() {
 				delimiter:";",
 				noheader: false,
 				ignoreEmpty:true,
-				headers:['VM-Policeninfo','Vermittler','Quittungs-Nr','Sprache','Anrede','Name','Vorname','Name2','Strasse','Postfach','PLZ/Ort','Policen-Nr1','Produkt1','Gewinn1','Policen-Nr2','Produkt2','Gewinn2','Policen-Nr3','Produkt3','Gewinn3','Policen-Nr4','Produkt4','Gewinn4','Policen-Nr5','Produkt5','Gewinn5','Policen-Nr6','Produkt6','Gewinn6','Policen-Nr7','Produkt7','Gewinn7','Policen-Nr8','Produkt8','Gewinn8','Policen-Nr9','Produkt9','Gewinn9','Policen-Nr10','Produkt10','Gewinn10','Policen-Nr11','Produkt11','Gewinn11','Policen-Nr12','Produkt12','Gewinn12','Policen-Nr13','Produkt13','Gewinn13','Policen-Nr14','Produkt14','Gewinn14','Policen-Nr15','Produkt15','Gewinn15','Policen-Nr16','Produkt16','Gewinn16','Policen-Nr17','Produkt17','Gewinn17','Policen-Nr18','Produkt18','Gewinn18','Policen-Nr19','Produkt19','Gewinn19','Policen-Nr20','Produkt20','Gewinn20','Policen-Nr21','Produkt21','Gewinn21','Policen-Nr22','Produkt22','Gewinn22','Policen-Nr23','Produkt23','Gewinn23','Policen-Nr24','Produkt24','Gewinn24','Policen-Nr25','Produkt25','Gewinn25','Summe Gewinn']
+				headers:['VM-Policeninfo','Vermittler','Quittungs-Nr','Sprache','Anrede','Name','Vorname','Name2','Strasse','Postfach','PLZ/Ort','Policen-Nr1','Produkt1','Gewinn1','Policen-Nr2','Produkt2','Gewinn2','Policen-Nr3','Produkt3','Gewinn3','Policen-Nr4','Produkt4','Gewinn4','Policen-Nr5','Produkt5','Gewinn5','Policen-Nr6','Produkt6','Gewinn6','Policen-Nr7','Produkt7','Gewinn7','Policen-Nr8','Produkt8','Gewinn8','Policen-Nr9','Produkt9','Gewinn9','Policen-Nr10','Produkt10','Gewinn10','Policen-Nr11','Produkt11','Gewinn11','Policen-Nr12','Produkt12','Gewinn12','Policen-Nr13','Produkt13','Gewinn13','Policen-Nr14','Produkt14','Gewinn14','Policen-Nr15','Produkt15','Gewinn15','Policen-Nr16','Produkt16','Gewinn16','Policen-Nr17','Produkt17','Gewinn17','Policen-Nr18','Produkt18','Gewinn18','Policen-Nr19','Produkt19','Gewinn19','Policen-Nr20','Produkt20','Gewinn20','Policen-Nr21','Produkt21','Gewinn21','Policen-Nr22','Produkt22','Gewinn22','Policen-Nr23','Produkt23','Gewinn23','Policen-Nr24','Produkt24','Gewinn24','Policen-Nr25','Produkt25','Gewinn25','Policen-Nr26','Produkt26','Gewinn26','Policen-Nr27','Produkt27','Gewinn27','Policen-Nr28','Produkt28','Gewinn28','Policen-Nr29','Produkt29','Gewinn29','Policen-Nr30','Produkt30','Gewinn30','Policen-Nr31','Produkt31','Gewinn31','Policen-Nr32','Produkt32','Gewinn32','Policen-Nr33','Produkt33','Gewinn33','Policen-Nr34','Produkt34','Gewinn34','Policen-Nr35','Produkt35','Gewinn35','Policen-Nr36','Produkt36','Gewinn36','Policen-Nr37','Produkt37','Gewinn37','Policen-Nr38','Produkt38','Gewinn38','Policen-Nr39','Produkt39','Gewinn39','Policen-Nr40','Produkt40','Gewinn40','Policen-Nr41','Produkt41','Gewinn41','Policen-Nr42','Produkt42','Gewinn42','Policen-Nr43','Produkt43','Gewinn43','Policen-Nr44','Produkt44','Gewinn44','Policen-Nr45','Produkt45','Gewinn45','Policen-Nr46','Produkt46','Gewinn46','Policen-Nr47','Produkt47','Gewinn47','Policen-Nr48','Produkt48','Gewinn48','Policen-Nr49','Produkt49','Gewinn49','Policen-Nr50','Produkt50','Gewinn50','Summe Gewinn']
 			}).fromFile(decodeFile).then(source => {
 				combine(source);
 			})
@@ -250,7 +325,7 @@ drop.addEventListener('drop', async (e) => {
 			delimiter:";",
 			noheader: false,
 			ignoreEmpty:true,
-			headers:['VM-Policeninfo','Vermittler','Quittungs-Nr','Sprache','Anrede','Name','Vorname','Name2','Strasse','Postfach','PLZ/Ort','Policen-Nr1','Produkt1','Gewinn1','Policen-Nr2','Produkt2','Gewinn2','Policen-Nr3','Produkt3','Gewinn3','Policen-Nr4','Produkt4','Gewinn4','Policen-Nr5','Produkt5','Gewinn5','Policen-Nr6','Produkt6','Gewinn6','Policen-Nr7','Produkt7','Gewinn7','Policen-Nr8','Produkt8','Gewinn8','Policen-Nr9','Produkt9','Gewinn9','Policen-Nr10','Produkt10','Gewinn10','Policen-Nr11','Produkt11','Gewinn11','Policen-Nr12','Produkt12','Gewinn12','Policen-Nr13','Produkt13','Gewinn13','Policen-Nr14','Produkt14','Gewinn14','Policen-Nr15','Produkt15','Gewinn15','Policen-Nr16','Produkt16','Gewinn16','Policen-Nr17','Produkt17','Gewinn17','Policen-Nr18','Produkt18','Gewinn18','Policen-Nr19','Produkt19','Gewinn19','Policen-Nr20','Produkt20','Gewinn20','Policen-Nr21','Produkt21','Gewinn21','Policen-Nr22','Produkt22','Gewinn22','Policen-Nr23','Produkt23','Gewinn23','Policen-Nr24','Produkt24','Gewinn24','Policen-Nr25','Produkt25','Gewinn25','Summe Gewinn']
+			headers:['VM-Policeninfo','Vermittler','Quittungs-Nr','Sprache','Anrede','Name','Vorname','Name2','Strasse','Postfach','PLZ/Ort','Policen-Nr1','Produkt1','Gewinn1','Policen-Nr2','Produkt2','Gewinn2','Policen-Nr3','Produkt3','Gewinn3','Policen-Nr4','Produkt4','Gewinn4','Policen-Nr5','Produkt5','Gewinn5','Policen-Nr6','Produkt6','Gewinn6','Policen-Nr7','Produkt7','Gewinn7','Policen-Nr8','Produkt8','Gewinn8','Policen-Nr9','Produkt9','Gewinn9','Policen-Nr10','Produkt10','Gewinn10','Policen-Nr11','Produkt11','Gewinn11','Policen-Nr12','Produkt12','Gewinn12','Policen-Nr13','Produkt13','Gewinn13','Policen-Nr14','Produkt14','Gewinn14','Policen-Nr15','Produkt15','Gewinn15','Policen-Nr16','Produkt16','Gewinn16','Policen-Nr17','Produkt17','Gewinn17','Policen-Nr18','Produkt18','Gewinn18','Policen-Nr19','Produkt19','Gewinn19','Policen-Nr20','Produkt20','Gewinn20','Policen-Nr21','Produkt21','Gewinn21','Policen-Nr22','Produkt22','Gewinn22','Policen-Nr23','Produkt23','Gewinn23','Policen-Nr24','Produkt24','Gewinn24','Policen-Nr25','Produkt25','Gewinn25','Policen-Nr26','Produkt26','Gewinn26','Policen-Nr27','Produkt27','Gewinn27','Policen-Nr28','Produkt28','Gewinn28','Policen-Nr29','Produkt29','Gewinn29','Policen-Nr30','Produkt30','Gewinn30','Policen-Nr31','Produkt31','Gewinn31','Policen-Nr32','Produkt32','Gewinn32','Policen-Nr33','Produkt33','Gewinn33','Policen-Nr34','Produkt34','Gewinn34','Policen-Nr35','Produkt35','Gewinn35','Policen-Nr36','Produkt36','Gewinn36','Policen-Nr37','Produkt37','Gewinn37','Policen-Nr38','Produkt38','Gewinn38','Policen-Nr39','Produkt39','Gewinn39','Policen-Nr40','Produkt40','Gewinn40','Policen-Nr41','Produkt41','Gewinn41','Policen-Nr42','Produkt42','Gewinn42','Policen-Nr43','Produkt43','Gewinn43','Policen-Nr44','Produkt44','Gewinn44','Policen-Nr45','Produkt45','Gewinn45','Policen-Nr46','Produkt46','Gewinn46','Policen-Nr47','Produkt47','Gewinn47','Policen-Nr48','Produkt48','Gewinn48','Policen-Nr49','Produkt49','Gewinn49','Policen-Nr50','Produkt50','Gewinn50','Summe Gewinn']
 		}).fromFile(decodeFile).then(source => {
 			combine(source);
 		})
